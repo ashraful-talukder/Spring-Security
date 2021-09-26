75fe0902daffa471d3b962b239f8a5c348cbd9ea
ashraful-talukder <ashraful.talukder24@gmail.com>
2021-09-26 18:08:51 +0600

    updated security

--- src/main/java/com/ash/springsecurity/email/EmailService.java
+++ src/main/java/com/ash/springsecurity/email/EmailService.java
@@ -30,7 +30,8 @@ public class EmailService implements EmailSender {
             helper.setText(email, true);
             helper.setTo((to));
             helper.setSubject("Confirm your email");
-            helper.setFrom("hello@ash.com");
+            helper.setFrom("ash.ashraful001@gmail.com");
+            mailSender.send(mimeMessage);
 
         } catch (MessagingException e){
             LOGGER.error("failed to send email", e);

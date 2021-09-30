0dc7d1603631d09a421174b7e2b1ea42336bc2f3
ashraful-talukder <ashraful.talukder24@gmail.com>
2021-09-30 17:19:33 +0600

    update

--- src/main/java/com/ash/springsecurity/registration/token/ConfirmationToken.java
+++ src/main/java/com/ash/springsecurity/registration/token/ConfirmationToken.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
@@ -41,7 +40,6 @@ public class ConfirmationToken {
     public ConfirmationToken(String token,
                              LocalDateTime createdAt,
                              LocalDateTime expiresAt,
-                             LocalDateTime confirmedAt,
                              AppUser appUser) {
         this.token = token;
         this.createdAt = createdAt;
